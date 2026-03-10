3434a65cc62cc639a508b99bc5eb25052fe299ce
LSalmada <lucas.almadag7@gmail.com>
2026-03-10 00:08:45 -0300

    create address pt2

--- src/entities/address.entity.ts
+++ src/entities/address.entity.ts
@@ -1,12 +1,14 @@
-export class Address {
+import { IAddress } from './models/address.interface'
+
+export class Address implements IAddress {
   id?: number
   street: string
   city: string
   state: string
-  zip_code: number
+  zip_code: string
   person_id?: number
 
-  constructor(street: string, city: string, state: string, zip_code: number) {
+  constructor(street: string, city: string, state: string, zip_code: string) {
     this.street = street
     this.city = city
     this.state = state
--- src/repositories/user.repository.interface.ts
+++ src/repositories/user.repository.interface.ts
@@ -1,7 +1,7 @@
-import { User } from '@/entities/user.entity'
-import { Person } from '@/entities/person.entity'
+import { IUser } from '@/entities/models/user.interface'
+import { IPerson } from '@/entities/models/person.interface'
 
 export interface IUserRepository {
-  findWithPerson(userId: number): Promise<(User & Person) | undefined>
-  create(user: User): Promise<User | undefined>
+  findWithPerson(userId: number): Promise<(IUser & IPerson) | undefined>
+  create(user: IUser): Promise<IUser | undefined>
 }
